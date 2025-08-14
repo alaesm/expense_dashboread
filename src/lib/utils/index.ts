7b08cf1767ab7c56119559a49267c69f53939e7b
alaesm <161771548+alaesm@users.noreply.github.com>
2025-08-14 23:41:48 +0200

    alae

--- src/lib/utils/index.ts
+++
@@ -1,7 +0,0 @@
-// Utility functions
-export * from './errorHandling';
-
-// Common utility function for combining classes (from shadcn/ui)
-export function cn(...classes: (string | undefined | null | false)[]): string {
-  return classes.filter(Boolean).join(' ');
-}
